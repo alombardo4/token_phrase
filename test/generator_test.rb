c83cb98da251d56d776ba2d8a3fd58329b6d74c9
Alec Lombardo <alec.lombardo@gmail.com>
2016-07-04 17:14:35 -0400

    Updated tests

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -10,8 +10,6 @@ class GeneratorTest < MiniTest::Unit::TestCase
   def test_that_permutations_calculate_all_combinations
     permutations = TokenPhrase.permutations(
       :adjectives => %w(test),
-      :colors => %w(test),
-      :patterns => %w(test),
       :nouns => %w(test),
       :numbers => false
     )
@@ -29,16 +27,6 @@ class GeneratorTest < MiniTest::Unit::TestCase
     assert_match /test/, phrase
   end
 
-  def test_that_colors_can_be_overridden
-    phrase = TokenPhrase.generate :colors => %w(test)
-    assert_match /test/, phrase
-  end
-  
-  def test_that_patterns_can_be_overridden
-    phrase = TokenPhrase.generate :patterns => %w(test)
-    assert_match /test/, phrase
-  end
-  
   def test_that_nouns_can_be_overridden
     phrase = TokenPhrase.generate :nouns => %w(test)
     assert_match /test/, phrase
@@ -46,12 +34,8 @@ class GeneratorTest < MiniTest::Unit::TestCase
 
   def test_that_dictionaries_can_be_added_to
     adjectives = TokenPhrase.adjectives %w(test)
-    colors = TokenPhrase.colors %w(test)
-    patterns = TokenPhrase.patterns %w(test)
     nouns = TokenPhrase.nouns %w(test)
     assert_equal (TokenPhrase.adjectives.count + 1), adjectives.count
-    assert_equal (TokenPhrase.colors.count + 1), colors.count
-    assert_equal (TokenPhrase.patterns.count + 1), patterns.count
     assert_equal (TokenPhrase.nouns.count + 1), nouns.count
   end
 end
